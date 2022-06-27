58e724b24e77f67c1a817ce6efbcc816a11a15b3
Gabriel Francis <69304692+gabrielzfr@users.noreply.github.com>
2022-06-26 22:49:27 -0300

    Initial commit

---
+++ src/lib/apollo.ts
@@ -0,0 +1,23 @@
+import { ApolloClient, DefaultOptions, InMemoryCache } from "@apollo/client";
+
+const defaultOptions: DefaultOptions = {
+
+
+  watchQuery: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "ignore",
+  },
+  query: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "all",
+  },
+};
+
+export const client = new ApolloClient({
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+      'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
+    cache: new InMemoryCache(),
+    defaultOptions: defaultOptions,
+});
