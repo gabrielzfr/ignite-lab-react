3a45cf18e7a87b6e551beefcf13ec114d6476df4
Gabriel Francis <69304692+gabrielzfr@users.noreply.github.com>
2022-06-26 23:03:38 -0300

    variavel do caminho do mockup.png

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -3,6 +3,7 @@ import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
+import imgUrl from '../../src/assets/code-mockup.png'
 
 
 export function Subscribe() {
@@ -55,7 +56,7 @@ export function Subscribe() {
                     </form>
                 </div>
             </div>
-            <img src="/src/assets/code-mockup.png" alt=""  className="mt-10"/>
+            <img src={imgUrl} alt=""  className="mt-10"/>
         </div>
     )
 }
